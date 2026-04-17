348cebd2aeaac982245fd494aece9396e89b7de9
Bhushan Baskar <bhushanbaskar3@gmail.com>
2026-04-17 20:39:36 +0530

    floyd's triangle basic program

---
+++ basics/lab_experiments/floyds_triangle.py
@@ -0,0 +1,20 @@
+'''
+problem statement:
+Control structures
+Floyd's triangle is a right-angled triangular array of natural numbers as
+shown below:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+Write a python program to print the Floyd‟s triangle
+'''
+
+count = 1
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(count,end=' ')
+        count +=1
+    print()
+    
